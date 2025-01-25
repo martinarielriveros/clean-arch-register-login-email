7ce1cdaacc2796fef571f1489b1e14da1fc1f3cb
martinarielriveros <martin.riveros@hotmail.com>
2025-01-25 07:50:29 -0300

    add tunnel instruction

--- src/config/regex-helpers.ts
+++ src/config/regex-helpers.ts
@@ -1,3 +1,10 @@
 export const customRegex = {
   email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+  phoneRegExp: /^(\+\d{1,3})?\d{10,14}$/,
+  nameRegex: /^[a-zA-Z\u00C0-\u00FF\s]*$/, // ? Only letters
+  cityRegex: /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]*$/,
+  latamDniExp: /^\d{7,9}$/,
+  streetNameRegex: /^[a-zA-Z0-9\u00C0-\u00FF ]+$/,
+  alphaNumericRegex: /^[a-zA-Z0-9]*$/,
+  numericRegex: /^[0-9]*$/,
 };
